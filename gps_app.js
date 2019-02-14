e343ce04bfd1246c61ff66547da3519632595826
leyihu61 <hlyca_melody@msn.com>
2019-02-14 11:33:37 -0800

    change to config at the beginning

--- gps_app.js
+++ gps_app.js
@@ -3,28 +3,27 @@ module.exports = function(RED) {
     	var gps_api = require('./gps_api.js');
     	var gps_data = require('./gps_data.js');
         RED.nodes.createNode(this,config);
+        // config before being triggered by input
+        gps_api.get_gps_config(function(data) {
+            if (data === 'Config does not exist') {
+                console.log("No config.");
+            }
+            if(data === null) {
+                console.log("Config read failed");
+            }
+            gps_api.configure_gps(function(configdata) {
+                console.log('config callback data: ' + configdata);
+            });
+        });
+
         var node = this;
         node.on('input', function(msg) {
-        	// get config
-        	gps_api.get_gps_config(function(data) {
-        		console.log("gps_conf:");
-        		console.log(data);
-        		if (data === 'Config does not exist') {
-        			console.log("No config.");
-        		}
-        		if(data === null) {
-        			console.log("Config read failed");
-        		}
-        		gps_api.configure_gps(function(configdata) {
-        			console.log(`config callback data: ${configdata}`);
-        			gps_data.get_gps_data(function(gpsdata) {
-        				console.log("=======================GPS Data:");
-                        console.log(gpsdata);
-        				msg.payload = JSON.stringify(gpsdata);
-        				node.send(msg);
-        			});
-        		});
-        	});            
+        	gps_data.get_gps_data(function(gpsdata) {
+                console.log("=======================GPS Data:");
+                console.log(gpsdata);
+                msg.payload = JSON.stringify(gpsdata);
+                node.send(msg);
+            });
         });
     }
 
